86c0b22adc80a83cf5e5a4ec10fede294593efb1
Matias Cao <caomatias@gmail.com>
2017-08-21 17:55:52 -0300

    Allow CORS on server

--- api/server.js
+++ api/server.js
@@ -2,20 +2,21 @@ var express = require('express'),
 app = express(),
 port = process.env.PORT || 3000,
 mongoose = require('mongoose'),
-Hotel = require('./api/models/hotelsListModel'), //created model loading here
-bodyParser = require('body-parser');
+Hotel = require('./api/models/hotelsListModel'),
+bodyParser = require('body-parser'),
+cors = require('cors');
 
-// mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Hoteldb'); 
 
+app.use(cors({origin: 'http://localhost:9000'}));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-var routes = require('./api/routes/hotelsListRoutes'); //importing route
-routes(app); //register the route
+var routes = require('./api/routes/hotelsListRoutes');
+routes(app);
 
 app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
