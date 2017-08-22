ff828c9937b11332473494c805afcffefb56fb17
Matias Cao <caomatias@gmail.com>
2017-08-22 00:55:56 -0300

    handle error

--- app/scripts/controllers/main.ctrl.js
+++ app/scripts/controllers/main.ctrl.js
@@ -2,11 +2,21 @@ app.controller('MainCtrl', ['$scope', 'hotels', function($scope, hotels) {
 
     $scope.hotels = [];
     $scope.hotelsCache = [];
+    $scope.hotelsError = false;
 
-    hotels.getHotels()
-        .then(function(res) {
-            $scope.hotels = res.data;
-            $scope.hotelsCache = res.data;
-        });
+    $scope.getHotels = function() {
+        hotels.getHotels()
+            .then(
+                function(res) {
+                $scope.hotels = res.data;
+                $scope.hotelsCache = res.data;
+                $scope.hotelsError = false;
+            },
+                function(res) {
+                    $scope.hotelsError = true;
+            })
+    }
+
+    $scope.getHotels();
         
 }]);
